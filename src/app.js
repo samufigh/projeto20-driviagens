d08b1ed32250de8079bc6cbdc5e313d6b6c98940
samufigh <samufigh21@gmail.com>
2023-09-05 11:15:39 -0300

    routes

---
+++ src/app.js
@@ -0,0 +1,12 @@
+import express from "express"
+import router from "./routes/index.routes.js"
+
+const app = express()
+
+app.use(express.json())
+app.use(router)
+
+const port = process.env.PORT || 5000
+app.listen(port, () => {
+	console.log(`Servidor rodando na porta ${port}`)
+})
---
+++ src/routes/travels.routes.js
@@ -0,0 +1,8 @@
+import { Router } from "express"
+
+const travelsRouter = Router()
+
+//cadastra uma viagem
+travelsRouter.post("/travels", )
+
+export default travelsRouter
