9c03ce99a93790d478507c97e51e323fb74b889b
samufigh <samufigh21@gmail.com>
2023-09-05 12:10:24 -0300

    fix: add register passenger function

--- src/controllers/passengers.controller.js
+++ src/controllers/passengers.controller.js
@@ -1,3 +1,13 @@
+export async function registerPassenger(req, res) {
+  try {
+    await //função
+    res.status(200).send("registerPassenger")
+  } catch (error) {
+    console.log(error);
+    return res.sendStatus(500);
+  }
+}
+
 export async function showPassengerTravels(req, res) {
     try {
       await //função
--- src/routes/passengers.routes.js
+++ src/routes/passengers.routes.js
@@ -1,8 +1,10 @@
 import { Router } from "express"
-import { showPassengerTravels } from "../controllers/passengers.controller.js"
+import { registerPassenger, showPassengerTravels } from "../controllers/passengers.controller.js"
 
 const passengersRouter = Router()
 
+//cadastra um passageiro
+passengersRouter.post("/passengers", registerPassenger)
 //cadastra uma cidade
 passengersRouter.get("/passengers/travels", showPassengerTravels)
 
