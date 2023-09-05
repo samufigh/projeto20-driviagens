0930ea958e93a9d0a0290a99a4447632316e7e78
samufigh <samufigh21@gmail.com>
2023-09-05 11:15:49 -0300

    routes

---
+++ src/routes/cities.routes.js
@@ -0,0 +1,10 @@
+import { Router } from "express"
+
+const citiesRouter = Router()
+
+//cadastra uma cidade
+citiesRouter.post("/cities", )
+
+export default citiesRouter
+
+
---
+++ src/routes/passengers.routes.js
@@ -0,0 +1,8 @@
+import { Router } from "express"
+
+const passengersRouter = Router()
+
+//cadastra uma cidade
+passengersRouter.get("/passengers/travels", )
+
+export default passengersRouter
